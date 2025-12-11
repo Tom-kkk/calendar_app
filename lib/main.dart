3830d371b035001bca6d4313bc69933da0fd7663
Tom-kkk <32155764@qq.com>
2025-12-11 17:06:25 +0800

    chore: setup riverpod and android desugaring

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:hive_flutter/hive_flutter.dart';
+import 'package:timezone/data/latest_all.dart' as tz;
 
-void main() {
-  runApp(const MainApp());
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  await Hive.initFlutter(); // 本地存储初始化
+  tz.initializeTimeZones(); // 加载时区数据，供本地通知使用
+
+  runApp(const ProviderScope(child: MainApp()));
 }
 
 class MainApp extends StatelessWidget {
